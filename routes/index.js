15852fee2b85d0a44ffe4dbee78dec2aecb53ad1
Taylor Cameron <taylorcameron95@hotmail.ca>
2018-10-31 02:41:25 +0000

    update to overall application to get it working

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+
+module.exports = function(app) {
+    app.get('/', function(req, res) {
+        res.render('pages/index');
+    });
+
+    app.get('/about', function(req, res) {
+        res.render('pages/about');
+    });
+};
+
--- server.js
+++ server.js
@@ -1,23 +1,14 @@
-‘use strict’;
-// ================================================================
-// get all the tools we need
-// ================================================================
-var express = require(‘express’);
-var routes = require(‘./routes/index.js’);
+var express = require('express');
+var routes = require('./routes/index.js');
 var port = process.env.PORT || 3000;
 var app = express();
-// ================================================================
-// setup our express application
-// ================================================================
-app.use(‘/public’, express.static(process.cwd() + ‘/public’));
-app.set(‘view engine’, ‘ejs’);
-// ================================================================
-// setup routes
-// ================================================================
+
+app.set("view engine", "ejs");
+app.use('/public', express.static(process.cwd() + '/public'));
+
+
 routes(app);
-// ================================================================
-// start our server
-// ================================================================
+
 app.listen(port, function() {
- console.log(‘Server listening on port ‘ + port + ‘…’);
+ console.log('Server listening on port ' + port + '…');
 });
